7cce946d61d10f5aa7e6d65466377cb9c4505b4a
Virgínia Menine Carvalho <31757372+Hannilock@users.noreply.github.com>
2021-04-20 22:29:58 -0300

    Patch v2

--- vi codigo/functions.js
+++ vi codigo/functions.js
@@ -1,15 +1,35 @@
+console.log("i worked :)");
 function msg() {
   alert("Formulário enviado!");
+  window.location.replace("../html/index.html");
 }
-function validateForm() {
-  var nome, cpf, email, mot, gen;
+function validateFormC() {
+  var nome, cpf, email;
   nome = document.getElementById('name').value;
   cpf = document.getElementById('cpf').value;
   email = document.getElementById('email').value;
-  if (nome == "" || cpf == "" || email == "") {
+  if(nome == "" || cpf == "" || email == "") {
     alert("Existem campos obrigatórios não preenchidos. Favor checar novamente");
     return false;
   }else{
   	msg();
+  }
+}
+function validateFormL() {
+  var nome, cpf, email, pass, confpass;
+  nome = document.getElementById('name').value;
+  cpf = document.getElementById('cpf').value;
+  email = document.getElementById('email').value;
+  pass = document.getElementById('senha').value;
+  confpass = document.getElementById('confirma').value;
+  if(nome == "" || cpf == "" || email == "" || pass == "" || confpass == ""){
+    alert("Existem campos obrigatórios não preenchidos. Favor checar novamente");
+    return false;
+  }else{
+  	if(pass != confpass){
+  		alert("As senhas não são iguais. Favor, digite novamente");
+  	}else{
+  		msg();
+  	}
   }
 }
